cb051ce707fbb4d19146ef619227ad246fa804ed
irre13 <afonuk@mail.ru>
2022-01-04 21:32:20 +0300

    feat:add index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  let reversed = number.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(number);
 }
+console.log(reverse(123));
